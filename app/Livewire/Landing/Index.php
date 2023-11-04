c78219f06ae494db8a5fbb5c61bf343eef98988e
Richie-Z <zakariarichie@yahoo.com>
2023-11-04 17:58:59 +0700

    enhance(landing): usage of wire model

--- app/Livewire/Landing/Index.php
+++ app/Livewire/Landing/Index.php
@@ -2,13 +2,52 @@
 
 namespace App\Livewire\Landing;
 
+use App\Repository\AgeRepository;
+use App\Repository\EducationRepository;
+use App\Repository\JobRepository;
+use App\Repository\QuestionRepository;
+use App\Repository\ServiceTypeRepository;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\View\View;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public Collection $ageDatas;
+    public Collection $educationDatas;
+    public Collection $jobDatas;
+    public Collection $serviceTypeDatas;
+    public Collection $questionDatas;
+
+    public string $name = "";
+    public string $phoneNumber = "";
+    public string $gender = "";
+    public string $ageId = "";
+    public string $jobId = "";
+    public string $job = "";
+    public string $serviceTypeId = "";
+
+    public function __construct(
+        protected AgeRepository $ageRepository = new AgeRepository(),
+        protected EducationRepository $educationRepository = new EducationRepository(),
+        protected JobRepository $jobRepository = new JobRepository(),
+        protected ServiceTypeRepository $serviceTypeRepository = new ServiceTypeRepository(),
+        protected QuestionRepository $questionRepository = new QuestionRepository(),
+    ) {
+    }
+
+    public function mount(): void
+    {
+        $this->ageDatas = $this->ageRepository->getAges();
+        $this->educationDatas = $this->educationRepository->getEducations();
+        $this->jobDatas = $this->jobRepository->getJobs();
+        $this->serviceTypeDatas = $this->serviceTypeRepository->getServiceTypes();
+        $this->questionDatas = $this->questionRepository->getQuestions();
+    }
+
     #[Layout('layouts.landing.app')]
-    public function render()
+    public function render(): View
     {
         return view('livewire.landing.index');
     }
--- resources/views/livewire/landing/index.blade.php
+++ resources/views/livewire/landing/index.blade.php
@@ -17,123 +17,108 @@
             </ul>
             <form class="tab-content" id="myTabContent">
                 <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab"
-                    tabindex="0">
+                    tabindex="0" x-data="{ job: '' }">
                     <div class="form-group mb-3">
                         <b>Nama Anda :</b>
-                        <input type="text" class="form-control" name="name">
+                        <input type="text" class="form-control" wire:model="name">
                     </div>
                     <div class="form-group mb-3">
                         <b>Nomor Hp :</b>
-                        <input type="text" class="form-control" name="phone_number">
+                        <input type="text" class="form-control" wire:model="phoneNumber">
                     </div>
                     <div class="form-group mb-3">
                         <b>Jenis Kelamin:</b>
-                        <select name="gender" id="" class="form-select">
-                            <option value="l">Laki-Laki</option>
-                            <option value="p">Perempuan</option>
+                        <select wire:model="gender" id="" class="form-select">
+                            <option value="male">Laki-Laki</option>
+                            <option value="female">Perempuan</option>
                         </select>
                     </div>
                     <div class="form-group mb-3">
                         <b>Usia Anda:</b>
-                        <select id="usia" name="age" class="form-select" required="">
+                        <select id="usia" wire:model="ageId" class="form-select" required="">
                             <option value="" disabled="" selected="">Pilih Usia Anda...</option>
-                            <option value="1">Kurang dari 20 Tahun</option>
-                            <option value="2">21 Sampai 25 Tahun</option>
-                            <option value="3">26 Sampai 30 Tahun</option>
-                            <option value="4">31 Sampai 35 Tahun</option>
-                            <option value="5">36 Sampai 40 Tahun</option>
-                            <option value="6">41 Sampai 45 Tahun</option>
-                            <option value="7">46 Sampai 50 Tahun</option>
-                            <option value="8">55 Sampai 60 Tahun</option>
-                            <option value="9">Lebih dari 60 Tahun</option>
+                            @foreach ($ageDatas as $data)
+                                <option value="{{ $data->id }}">{{ $data->name }}</option>
+                            @endforeach
                         </select>
                     </div>
                     <div class="form-group mb-3">
                         <b>Pendidikan:</b>
-                        <select id="pendidikan" name="education" class="form-select" required="">
+                        <select id="pendidikan" wire:model="educationId" class="form-select" required="">
                             <option value="" disabled="" selected="">Pilih Pendidikan Terakhir
                                 Anda...</option>
-                            <option value="1">SD</option>
-                            <option value="2">SMP</option>
-                            <option value="3">SMA</option>
-                            <option value="4">DIPLOMA 3</option>
-                            <option value="5">SARJANA</option>
-                            <option value="6">PASCASARJANA / LEBIH</option>
+                            @foreach ($educationDatas as $data)
+                                <option value="{{ $data->id }}">{{ $data->name }}</option>
+                            @endforeach
                         </select>
                     </div>
                     <div class="form-group mb-3">
                         <b>Pekerjaan:</b>
-                        <select id="pendidikan" name="job" class="form-select" required="">
+                        <select id="pendidikan" wire:model="job" x-model="job" class="form-select" required="">
                             <option value="" disabled="" selected="">Pilih Pekerjaan Anda...
                             </option>
-                            <option value="1">PNS</option>
-                            <option value="2">TNI</option>
-                            <option value="3">POLRI</option>
-                            <option value="4">Swasta</option>
-                            <option value="5">Wiraswasta</option>
-                            <option value="6">Lainnya</option>
+                            @foreach ($jobDatas as $data)
+                                <option value="{{ $data->name }}">
+                                    {{ $data->name }}</option>
+                            @endforeach
+                            <option value="etc">Lainnya</option>
                         </select>
                     </div>
-                    <div class="form-group mb-3">
+                    <div class="form-group mb-3" x-show="job == 'etc'">
                         <b>Masukkan Pekerjaan Lainnya</b>
-                        <input type="text" class="form-control" name="other_job">
+                        <input type="text" class="form-control" wire:model="job">
                     </div>
                     <div class="form-group ">
                         <b>Jenis Layanan:</b>
-                        <select id="pendidikan" name="service_type" class="form-select" required="">
+                        <select id="pendidikan" wire:model="serviceTypeId" class="form-select" required="">
                             <option value="" disabled="" selected="">Pilih Jenis Layanan...
                             </option>
-                            <option value="1">Rawat Jalan</option>
-                            <option value="2">Rawat Inap</option>
-                            <option value="3">IGD</option>
-                            <option value="4">Instalasi Farmasi</option>
-                            <option value="5">Instalasi Rehabilitas Medik</option>
-                            <option value="6">Laboratorium</option>
-                            <option value="7">Instalasi Radiologi</option>
-                            <option value="8">Ruang Bersalin dan Peristi</option>
-                            <option value="9">Bagian Administrasi</option>
+                            @foreach ($serviceTypeDatas as $data)
+                                <option value="{{ $data->id }}">{{ $data->name }}</option>
+                            @endforeach
                         </select>
                     </div>
                 </div>
                 <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab"
                     tabindex="0">
-                    <div class="form-group mb-3">
-                        <p>1. Bagaimana pendapat Saudara tentang kesesuaian persyaratan pelayanan dengan jenis
-                            pelayanannya</p>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
-                                value="option1" checked>
-                            <label class="form-check-label" for="exampleRadios1">
-                                Tidak Sesuai
-                            </label>
+                    @foreach ($questionDatas as $key => $data)
+                        <div class="form-group mb-3">
+                            <p>{{ $key + 1 . '. ' . $data->name }}</p>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="exampleRadios"
+                                    id="exampleRadios1" value="option1" checked>
+                                <label class="form-check-label" for="exampleRadios1">
+                                    Tidak Sesuai
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="exampleRadios"
+                                    id="exampleRadios2" value="option2">
+                                <label class="form-check-label" for="exampleRadios2">
+                                    Kurang Sesuai
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="exampleRadios"
+                                    id="exampleRadios2" value="option2">
+                                <label class="form-check-label" for="exampleRadios2">
+                                    Sesuai
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="exampleRadios"
+                                    id="exampleRadios2" value="option2">
+                                <label class="form-check-label" for="exampleRadios2">
+                                    Sangat Sesuai
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="exampleRadios"
+                                    id="exampleRadios2" value="lainnya">
+                                <input type="text" placeholder="Lainnya">
+                            </div>
                         </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2"
-                                value="option2">
-                            <label class="form-check-label" for="exampleRadios2">
-                                Kurang Sesuai
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2"
-                                value="option2">
-                            <label class="form-check-label" for="exampleRadios2">
-                                Sesuai
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2"
-                                value="option2">
-                            <label class="form-check-label" for="exampleRadios2">
-                                Sangat Sesuai
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2"
-                                value="lainnya">
-                            <input type="text" placeholder="Lainnya">
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </form>
         </div>
